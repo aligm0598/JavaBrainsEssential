6088b5500208f50a1a15374240197a5d5177ce6e
John Li <aligm0598@yahoo.ca>
2023-08-02 10:06:03 -0400

    arrays

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int slNos[] = new int[10];
+        slNos[0] = 2;
+        slNos[3] = 100;
+
+        int val = slNos[0]; 
+        System.out.println("slNos[0]: " + slNos[0]);
+        System.out.println("slNos[3]: " + slNos[3]);
+        System.out.println("val: " + val);
+    }
+}
