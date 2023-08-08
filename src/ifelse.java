99085868ed95de491a90999e304c64f1965153ea
John Li <aligm0598@yahoo.ca>
2023-08-08 10:23:26 -0400

    if else

---
+++ src/ifelse.java
@@ -0,0 +1,15 @@
+public class ifelse {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        if (a > b) {
+            System.out.println("A is greater");
+        } else if (a < b) {
+            System.out.println("A is lesser");
+        } else {
+            System.out.println("They are equal");
+        }
+    }
+}
