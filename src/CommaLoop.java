6c0a384ede1e248605053d0ddf05c00b5966190b
John Li <aligm0598@yahoo.ca>
2023-08-09 09:51:45 -0400

    Switch and comma

---
+++ src/CommaLoop.java
@@ -0,0 +1,8 @@
+public class CommaLoop {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0, j = 10; i < 10; i++, j--) {
+            System.out.println(i + j);
+        }
+    }
+}
---
+++ src/SwitchExpressions.java
@@ -0,0 +1,12 @@
+public class SwitchExpressions{
+    public static void main(String[] args) {
+        String day = "MONDAY"; 
+        int numLetters = switch (day) {
+            case "MONDAY", "FRIDAY", "SUNDAY" -> 6;
+            case "TUESDAY", "THURSDAY", "SATURDAY" -> 7;
+            case "WEDNESDAY" -> 9;
+            default -> 0; 
+        };
+        System.out.println("Number of letters: " + numLetters);
+    }
+}
