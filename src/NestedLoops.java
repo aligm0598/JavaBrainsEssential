818bb831727c1feba89cafb58bf5f1c3bef5ccf4
John Li <aligm0598@yahoo.ca>
2023-08-10 15:02:26 -0400

    Highest number

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,11 +1,21 @@
-public class NestedLoops{
+public class NestedLoops {
     public static void main(String[] args) {
-        int matrix[][] = new int[10][20]; 
+        int matrix[][] = new int[10][20];
+        
+        int minNumber = 10;  
+        int maxNumber = 50;  
 
-        for (int[] subArray : matrix) {
-            for (int element : subArray) {
-                System.out.println("Element is: " + element); 
-            }
-        }
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                matrix[i][j] = minNumber + (int) (Math.random() * (maxNumber - minNumber + 1));
+
+        int maxInMatrix = matrix[0][0];
+
+        for (int[] subArray : matrix)
+            for (int element : subArray)
+                maxInMatrix = Math.max(maxInMatrix, element);
+
+        System.out.println("Max number in the matrix: " + maxInMatrix);
     }
 }
+
