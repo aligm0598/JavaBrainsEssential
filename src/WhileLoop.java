22323382ebe9fc2cca6575975c2e75db24e6cb38
John Li <aligm0598@yahoo.ca>
2023-08-15 10:54:50 -0400

    While loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
+}
