17b53b39274f4586e7c36a92c7e0dd3e4f768072
John Li <aligm0598@yahoo.ca>
2023-08-15 11:00:10 -0400

    Break and countiune

---
+++ src/BreakContinue.java
@@ -0,0 +1,9 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                continue;//You can change this 
+            System.out.println(i);
+        }
+    }
+}
