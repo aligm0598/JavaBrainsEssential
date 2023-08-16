6696de2ca34f04aaa623d3a0dcfc32cd73b629f4
John Li <aligm0598@yahoo.ca>
2023-08-16 14:30:51 -0400

    larger number

--- src/ForEach.java
+++ src/ForEach.java
@@ -2,9 +2,17 @@ public class ForEach {
     public static void main(String[] args) {
         int someNumbers[] = { 25, 42, 36, 75, 91 };
         int total = 0;
-        for (var element : someNumbers)
+        int maxNumber = Integer.MIN_VALUE; 
+
+        for (var element : someNumbers) {
             total = total + element;
+            
+            if (element > maxNumber) {
+                maxNumber = element;
+            }
+        }
 
         System.out.println("Total: " + total);
+        System.out.println("Max Number: " + maxNumber);
     }
 }
